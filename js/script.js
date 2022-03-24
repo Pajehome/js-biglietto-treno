09d9d2787cbcf8f13b0e6dfbe7fd9d189ffaa850
Pajehome <abdel20069@gmail.com>
2022-03-24 15:11:31 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -4,3 +4,45 @@
 // va applicato uno sconto del 20% per i minorenni
 // va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+
+let userKm = parseInt(prompt('Inserisci Km'));
+
+console.log(userKm);
+
+let userAge = parseInt(prompt('Inserisci età'));
+
+console.log(userAge);
+
+const perKm = 0.21;
+
+const u18 = 0.2;
+
+const o65 = 0.4;
+
+let totaleKm = userKm * perKm;
+
+console.log(totaleKm);
+
+if(userAge <= 18){
+
+    console.log('Sconto 20% applicato');
+
+    let totalUnder18 = totaleKm - (totaleKm * u18);
+
+    totalUnder18 = totalUnder18.toFixed(2);
+ 
+    console.log(totalUnder18);
+
+}else if(userAge >= 65){
+
+    console.log('Sconto 40% applicato')
+
+    let totalOver65 = totaleKm - (totaleKm * o65);
+
+    totalOver65 = totalOver65.toFixed(2);
+ 
+    console.log(totalOver65);
+
+}
+
+
